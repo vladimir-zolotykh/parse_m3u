5e6e6a4b59fa5ea0ccdb67eb6ad86eeb0cd58aed
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-05-06 19:06:41 +0300

    Initial import
    Parse .m3u file

---
+++ parse_m3u.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# PYTHON_ARGCOMPLETE_OK
+from collections import namedtuple
+
+
+Song = namedtuple("Song", "title seconds filename")
+
+
+def parse_m3u():
+    pass
+
+
+if __name__ == "__main__":
+    parse_m3u()

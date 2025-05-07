e10e1efaf08bbc6ed37b58af3a0579670b87e8e0
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-05-07 21:20:08 +0300

    Initial import
    m3u_bnf() function. Parse .m3u playlist with pyparsing

---
+++ parse_m3u_pp.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# PYTHON_ARGCOMPLETE_OK
+import pyparsing as pp
+
+
+def m3u_bnf() -> pp.core.ParserElement:
+    hdr = pp.Suppress("#EXTM3U")
+    filename = pp.restOfLine.set_results_name("filename")
+    title = pp.restOfLine.set_results_name("title")
+    seconds = pp.Regex(r"-?\d+").set_results_name("seconds")
+    info = pp.Suppress("#EXTINF:") + seconds + pp.Suppress(",") + title
+    nl = pp.LineEnd().suppress()
+    entry = info + nl + filename + nl
+    m3u = hdr + nl + pp.OneOrMore(entry)
+    return m3u
+
+
+if __name__ == "__main__":
+    with open("Various-Pop.m3u") as fh:
+        d = m3u_bnf().parse_file(fh)
+    print(d)

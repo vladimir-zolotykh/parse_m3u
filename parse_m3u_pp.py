4b277ec14394c3c96b78163ae07cebd0f72734a3
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-05-09 08:53:01 +0300

    Make parse_m3u_fh() parse TextIO, Path, or str

--- parse_m3u_pp.py
+++ parse_m3u_pp.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
 """
->>> parse_m3u_file(Path("Various-Pop.m3u"))
+>>> parse_m3u_fh(Path("Various-Pop.m3u"))
 ParseResults([ParseResults(['236', 'Various - Two Tribes', 'Various/Frankie Goes To Hollywood/02-Two Tribes.ogg'], {'seconds': '236', 'title': 'Various - Two Tribes', 'filename': 'Various/Frankie Goes To Hollywood/02-Two Tribes.ogg'}), ParseResults(['237', 'Various - Relax', 'Various/Frankie Goes To Hollywood/01-Relax.ogg'], {'seconds': '237', 'title': 'Various - Relax', 'filename': 'Various/Frankie Goes To Hollywood/01-Relax.ogg'}), ParseResults(['330', 'Various - The Power of Love', 'Various/Frankie Goes To Hollywood/12-The Power of Love.ogg'], {'seconds': '330', 'title': 'Various - The Power of Love', 'filename': 'Various/Frankie Goes To Hollywood/12-The Power of Love.ogg'}), ParseResults(['-1', 'Various - Material Girl', 'Various/Madonna/05-Material Girl.ogg'], {'seconds': '-1', 'title': 'Various - Material Girl', 'filename': 'Various/Madonna/05-Material Girl.ogg'}), ParseResults(['303', 'The Police - Walking On The Moon', 'Various/Sting & The Police 1997/06-Walking On The Moon.ogg'], {'seconds': '303', 'title': 'The Police - Walking On The Moon', 'filename': 'Various/Sting & The Police 1997/06-Walking On The Moon.ogg'})], {})
 """
 from typing import TextIO
@@ -22,13 +22,16 @@ def m3u_bnf() -> pp.core.ParserElement:
     return m3u
 
 
-def _parse_m3u_fh(fh: TextIO) -> pp.ParseResults:
-    return m3u_bnf().parse_file(fh)
+def parse_m3u_fh(fh_or_str: TextIO | Path | str) -> pp.ParseResults:
+    """Parse TextIO, Path, or str"""
 
-
-def parse_m3u_file(filename: str | Path) -> pp.ParseResults:
-    with open(filename) as fh:
-        return _parse_m3u_fh(fh)
+    if isinstance(fh_or_str, (Path, str)):
+        with open(fh_or_str) as fh:
+            return m3u_bnf().parse_file(fh)
+    elif isinstance(fh_or_str, TextIO):
+        return m3u_bnf().parse_file(fh_or_str)
+    else:
+        raise TypeError(f"{fh_or_str}: must be TextIO, Path, or str")
 
 
 if __name__ == "__main__":

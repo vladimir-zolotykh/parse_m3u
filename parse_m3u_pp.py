fdbed089e9f85da0592a710e3e6bc99955e9b200
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-05-09 08:41:44 +0300

    Convert into doctest

--- parse_m3u_pp.py
+++ parse_m3u_pp.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
+"""
+>>> parse_m3u_file(Path("Various-Pop.m3u"))
+ParseResults([ParseResults(['236', 'Various - Two Tribes', 'Various/Frankie Goes To Hollywood/02-Two Tribes.ogg'], {'seconds': '236', 'title': 'Various - Two Tribes', 'filename': 'Various/Frankie Goes To Hollywood/02-Two Tribes.ogg'}), ParseResults(['237', 'Various - Relax', 'Various/Frankie Goes To Hollywood/01-Relax.ogg'], {'seconds': '237', 'title': 'Various - Relax', 'filename': 'Various/Frankie Goes To Hollywood/01-Relax.ogg'}), ParseResults(['330', 'Various - The Power of Love', 'Various/Frankie Goes To Hollywood/12-The Power of Love.ogg'], {'seconds': '330', 'title': 'Various - The Power of Love', 'filename': 'Various/Frankie Goes To Hollywood/12-The Power of Love.ogg'}), ParseResults(['-1', 'Various - Material Girl', 'Various/Madonna/05-Material Girl.ogg'], {'seconds': '-1', 'title': 'Various - Material Girl', 'filename': 'Various/Madonna/05-Material Girl.ogg'}), ParseResults(['303', 'The Police - Walking On The Moon', 'Various/Sting & The Police 1997/06-Walking On The Moon.ogg'], {'seconds': '303', 'title': 'The Police - Walking On The Moon', 'filename': 'Various/Sting & The Police 1997/06-Walking On The Moon.ogg'})], {})
+"""
 from typing import TextIO
 from pathlib import Path
 import pyparsing as pp
@@ -28,4 +32,6 @@ def parse_m3u_file(filename: str | Path) -> pp.ParseResults:
 
 
 if __name__ == "__main__":
-    print(parse_m3u_file(Path("Various-Pop.m3u")))
+    import doctest
+
+    doctest.testmod()
